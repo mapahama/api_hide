81bb78cf2cbd34769dfa98e9dfd02235de887d95
mapahama <nodrik00@abv.bg>
2023-05-27 11:44:49 +0200

    Add files via upload

---
+++ answers.js
@@ -0,0 +1,12 @@
+// answers, which aim to indicate  the identity of the personal assistant "Api"
+let intro = ["I am your digital assistant Api!", "Hi, I am Api!", "My name is Api",
+ "I am your personal assistent Api!", "I am machine, named Api!"];
+
+ let help =["how may I assistant you", "how I can halp you", "what can I do for you?"];
+
+ let thanks = ["most welcome", "as you wish"];
+
+
+
+
+ 
---
+++ gptTurbo.js
@@ -0,0 +1,52 @@
+
+
+// need to be hidden and not exposed in a client code !
+//const API_KEY = "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";
+//process.env.API_KEY
+const apikey = process.env.API_KEY;
+var myTimeout;
+
+/*
+*Using OpenAI API to send user questions to gpt-3.5-turbo model and return answers.
+*@param input:  user question
+*@return result1: answer from gpt model
+*/
+async function getGPTmessage(input) {
+
+  var result1 = null;
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${apikey}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: input}],
+      max_tokens: 300
+    })
+  }
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    console.log(data);
+
+    result1 = data.choices[0].message.content;
+  
+  } catch (error){
+    console.log(error);
+  }
+  return result1;
+
+}
+
+
+
+
+// pause and resume speech-synthesis every 10 sec to avoid speech-synthesis errors in long texts
+function myTimer() {
+    window.speechSynthesis.pause();
+    window.speechSynthesis.resume();
+    myTimeout = setTimeout(myTimer, 10000);
+}
